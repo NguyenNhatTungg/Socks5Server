b082993d125b816fe8889edbce04e76daaf1fac0
NguyenNhatTungg <qswavippro@gmail.com>
2024-05-26 23:05:28 +0700

    create Client

--- Socks5Server/TCP/Client.cs
+++ Socks5Server/TCP/Client.cs
@@ -21,5 +21,72 @@ namespace Socks5Server.TCP
             buffer = new byte[packetSize];
             sock.ReceiveBufferSize = packetSize;
         }
+
+        public event EventHandler<DataEventArgs> OnDataReceived = delegate { };
+        public event EventHandler<DataEventArgs> OnDataSent = delegate { };
+
+        public void disconnect()
+        {
+            try
+            {
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+        public bool send(byte[] buffer)
+        {
+            return (send(buffer, 0, buffer.Length));
+        }
+
+        public bool send(byte[] buffer, int offset, int count)
+        {
+            try
+            {
+                if (socket != null)
+                {
+                    if (socket.Send(buffer, offset, count, SocketFlags.None) <= 0)
+                    {
+                        disconnect();
+                        return false;
+                    }
+
+                    var data = new DataEventArgs(this, buffer, count);
+                    OnDataSent(this, data);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                disconnect();
+                return false;
+            }       
+        }
+
+        public int receive(byte[] data, int offset, int count)
+        {
+            try
+            {
+                var received = socket.Receive(data, offset, count, SocketFlags.None);
+                if (received <= 0)
+                {
+                    disconnect();
+                    return -1;
+                }
+
+                return received;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                disconnect();
+                return -1;
+            }
+        }
     }
 }
